3addcf15de7f14ef657c77ca23db43e7742c4f94
Administrator <962836309@qq.com>
2019-03-14 21:05:07 +0800

    1.0

--- taotao-parent/taotao-manager/taotao-manager-service/src/test/java/com/taotao/AppTest.java
+++ taotao-parent/taotao-manager/taotao-manager-service/src/test/java/com/taotao/AppTest.java
@@ -7,14 +7,35 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.dao.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testPageHelper() throws Exception {
+        //初始化spring容器
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+        //获得Mapper的代理对象
+        TbItemMapper itemMapper = applicationContext.getBean(TbItemMapper.class);
+        //设置分页信息
+        PageHelper.startPage(1, 30);
+        //执行查询
+        TbItemExample example = new TbItemExample();
+        List<TbItem> list = itemMapper.selectByExample(example);
+        //取分页信息
+        PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+        System.out.println(pageInfo.getTotal());
+        System.out.println(pageInfo.getPages());
+        System.out.println(pageInfo.getPageNum());
+        System.out.println(pageInfo.getPageSize());
     }
 }
+
