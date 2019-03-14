d195baf0877968808b78d59e49ef355ba309fc43
Administrator <962836309@qq.com>
2019-03-14 21:10:42 +0800

    2.0

---
+++ taotao-parent/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String showIndex(){
+    return "index";
+    }
+    /**
+     * 展示菜单页面
+     * @param page
+     * @return
+     */
+    @RequestMapping("/{page}")
+    public String showItemList(@PathVariable String page){
+        return page;
+    }
+}
